e72ccf67a4007dd624afe678ad71df2b0f13e067
calel <calebjuan44@gmail.com>
2026-08-09 18:54:31 -0500

    probando el scm cada 5min muajaja

--- src/main/java/com/unalm/matricula_api/MatriculaApiApplication.java
+++ src/main/java/com/unalm/matricula_api/MatriculaApiApplication.java
@@ -8,6 +8,7 @@ public class MatriculaApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatriculaApiApplication.class, args);
+		System.out.println("Hola mi amada");
 	}
 
 }
